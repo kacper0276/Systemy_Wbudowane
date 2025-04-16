7f137cf33d47156edeb738a528deecee492182a0
Kacper Renkel <72521677+kacper0276@users.noreply.github.com>
2025-04-16 10:04:55 +0200

    Ex 4

---
+++ Ćwiczenia_4/zad_1.c
@@ -0,0 +1,39 @@
+/*
+ * File:   zad_1.c
+ * Author: kacpe
+ *
+ * Created on April 9, 2025, 10:49 AM
+ */
+
+// CONFIG2
+#pragma config POSCMOD = XT             // Primary Oscillator Select (XT Oscillator mode selected)
+#pragma config OSCIOFNC = ON            // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as port I/O (RC15))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRI              // Oscillator Select (Primary Oscillator (XT, HS, EC))
+#pragma config IESO = ON                // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) enabled)
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = ON              // Watchdog Timer Enable (Watchdog Timer is enabled)
+#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+
+#include "xc.h"
+
+// Miga co druga dioda
+
+int main(void) {
+    unsigned char portValue = 0x55; // zapala sie co druga dioda
+    
+    AD1PCFG = 0xFFFF;
+    
+    TRISA = 0x0000; // port a na wyjscie
+    
+    LATA = portValue;
+    
+    return 0;
+}
